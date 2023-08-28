aa1c8182790203506a544d8b4e0c2b80a328cd6a
JKKholmatov <XKholmatov@gmail.com>
2023-08-28 17:13:43 +0500

    initial

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [loading, setLoading] = useState(false);
+
+  const fetchJson = async () => {
+    setLoading(true);
+    console.log(new Date().toLocaleTimeString() + " Fetch start");
+    await fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+    console.log(new Date().toLocaleTimeString() + " Fetch End");
+    setLoading(false);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity style={styles.button} onPress={() => fetchJson()}>
+        {loading ? <ActivityIndicator /> : <Text>Fetch Json</Text>}
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +32,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    height: 50,
+    width: 100,
+    borderRadius: 15,
+    backgroundColor: "#007AFF",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
